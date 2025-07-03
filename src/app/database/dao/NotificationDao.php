2321b430baba66555d4952c21bf6c400739b624c
Ankio <ankio@ankio.net>
2025-07-03 14:14:58 +0800

    :sparkles: (db/api/ui): 添加通知链接功能并优化路由
    - 在NotificationModel中添加link字段以支持通知跳转链接
    - 修改publish路由路径并新增UI界面路由
    - 在NotificationDao中更新post方法以处理link参数
    - 添加删除过期通知的清理方法deleteTimeOut()

--- src/app/database/dao/NotificationDao.php
+++ src/app/database/dao/NotificationDao.php
@@ -10,16 +10,22 @@ class NotificationDao extends Dao
     function getUnread($since_ts): array
     {
         return $this->select()->where(["is_read" => false, "t > $since_ts"])->commit();
-
     }
 
-    function post($title,$content): void
+    function post($title,$content,$link): void
     {
         $model = new NotificationModel();
         $model->title = $title;
         $model->content = $content;
+        $model->link = $link;
         $model->is_read = false;
         $model->t = time();
         $this->insertModel($model);
+        $this->deleteTimeOut();
+    }
+
+    function deleteTimeOut(): void
+    {
+        $this->delete()->where(["t < ".(time()-3600*365)])->commit();
     }
 }
--- src/app/database/model/NotificationModel.php
+++ src/app/database/model/NotificationModel.php
@@ -10,5 +10,8 @@ class NotificationModel extends Model
     public string $content  = "";    // 富文本内容（HTML / Markdown）
     public bool   $is_read  = false; // 是否已读
 
+    public string $link = "";// 跳转链接
+
+
     public int $t = 0;//发布时间
 }
