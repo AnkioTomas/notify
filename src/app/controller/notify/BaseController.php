a441c93375e3ac9fcf7dc93cab5aa0830a913833
Ankio <ankio@ankio.net>
2025-07-11 15:24:24 +0800

    :truck: (controller): 将UI控制器移动到notify命名空间
    将BaseController和Main控制器从ui命名空间迁移到notify命名空间，以更好地反映其实际功能用途

---
+++ src/app/controller/notify/BaseController.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\controller\notify;
+
+use nova\framework\http\Response;
+use nova\framework\route\Controller;
+use nova\plugin\cookie\Session;
+use nova\plugin\login\db\Model\UserModel;
+use nova\plugin\login\LoginManager;
+
+class BaseController extends Controller
+{
+    protected ?UserModel $userModel;
+    public function init(): ?Response
+    {
+        Session::getInstance()->start(365 * 24 * 60 * 60, "ankio_notify");
+        $this->userModel = LoginManager::getInstance()->checkLogin();
+        if (empty($this->userModel)) {
+            $this->redirectTo(LoginManager::getInstance()->redirectLogin());
+        }
+        return null;
+
+    }
+
+    protected function redirectTo($link): Response
+    {
+        if (!$this->request->isPjax()) {
+            //跳转后台
+            return Response::asRedirect($link);
+        } else {
+            return Response::asHtml(
+                <<<EOF
+<title id="title">302 Redirect</title>
+<style id="style"></style>
+<div id="container" class="container"></div>
+<script id="script">
+    window.pageLoadFiles = [];
+    window.pageOnLoad = function (loading) {
+        location.replace('$link');
+        return false
+    };
+</script>
+EOF
+            );
+        }
+    }
+}
--- src/app/controller/ui/Main.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace app\controller\ui;
-
-class Main extends BaseController
-{
-}
